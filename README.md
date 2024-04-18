0d28729ca9dd9833d4197647f8fffa079f2e5fa3
Dalufishe <fishawe87@gmail.com>
2024-04-19 07:37:54 +0800

    first-commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# Palserver Map
+
+Version 0.0.1, currently featureless, but will get better over time.
+
+### How to use
+
+#### Run server
+
+```bash
+cd backend
+npm i
+node server.js
+```
+
+#### Run Web page
+
+```bash
+cd frontend
+```
+
+Using vscode live server to start it.
+
+#### Start use the map
+
+```bash
+http://127.0.0.1:5501/frontend/?ip={your ip address}&port={rest api public port}&password={password for admin}
+```
---
+++ backend/server.js
@@ -0,0 +1,51 @@
+const express = require("express");
+const cors = require("cors");
+const axios = require("axios");
+
+const app = express();
+const port = 3333;
+
+app.use(cors());
+
+app.get("/players", async (req, res) => {
+  const { ip, port, password } = req.query;
+
+  try {
+    const result = await axios(
+      "http://" + ip + ":" + port + `/v1/api/players`,
+      {
+        method: "get",
+        auth: {
+          username: "admin",
+          password: password,
+        },
+      }
+    );
+
+    res.send(result.data || {});
+  } catch (e) {
+    res.send({});
+  }
+});
+
+app.get("/info", async (req, res) => {
+  const { ip, port, password } = req.query;
+
+  try {
+    const result = await axios("http://" + ip + ":" + port + `/v1/api/info`, {
+      method: "get",
+      auth: {
+        username: "admin",
+        password: password,
+      },
+    });
+
+    res.send(result.data || {});
+  } catch (e) {
+    res.send({});
+  }
+});
+
+app.listen(port, () => {
+  console.log("palserver Map is running on " + port);
+});
